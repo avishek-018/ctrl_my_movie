48bdb9ded33a31195b78ebe9a39e2e40bf65bc40
silenthunter007 <avishek.das.ayan@gmail.com>
2019-05-28 23:02:20 +0600

    Add files via upload

---
+++ controller.py
@@ -0,0 +1,28 @@
+
+import sys
+import time
+from firebase import firebase
+dbase = 'https://key-press.firebaseio.com/'
+firebase = firebase.FirebaseApplication( dbase, None)
+from pynput.keyboard import Key, Listener
+
+control_list = ['Key.space', 'Key.up', 'Key.down', 'Key.left', 'Key.right']
+def counter():
+    if 'cnt' not in counter.__dict__:
+        counter.cnt = 0
+    counter.cnt += 1
+    return counter.cnt
+
+def keypress(Key):
+    if str(Key) == 'Key.esc':
+        sys.exit()
+    elif str(Key) not in control_list:
+        pass
+    else:
+        firebase.put('/key-press/', 'counter', counter())
+        firebase.put('/key-press/', 'key_data', str(Key))
+
+with Listener(on_press = keypress) as listener:
+        listener.join()
+
+ 
---
+++ simulator.py
@@ -0,0 +1,29 @@
+
+# coding: utf-8
+
+# In[20]:
+
+
+import sys,time
+from firebase import firebase
+from pynput.keyboard import Key, Controller
+
+dbase = 'https://key-press.firebaseio.com/'
+
+firebase = firebase.FirebaseApplication( dbase, None)
+
+keyboard = Controller()
+
+
+counter = firebase.get('/key-press/counter/', '')
+while(1):
+    counter_up = firebase.get('/key-press/counter/', '')
+    if counter != counter_up:
+        counter = counter_up
+        key_data = firebase.get('/key-press/key_data/', '')
+        if key_data == 'Key.esc':
+            sys.exit() 
+        else:
+            keyboard.press(eval(key_data))
+            keyboard.release(eval(key_data))
+
